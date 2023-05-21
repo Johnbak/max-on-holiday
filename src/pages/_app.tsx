f163a99d4341a466e993f442f6364374f806f97a
knowdeep <worstgrim@gmail.com>
2023-05-21 23:59:49 +0700

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div>
+    <Head>
+      <title>Active On Holiday</title>
+      <meta name="description" content="Ez Web Page" />
+      <link rel="stylesheet" href="./node_modules/nes.css/css/nes.min.css" />
+      <link href="https://unpkg.com/nes.css/css/nes.css" rel="stylesheet" />
+    </Head>
+    <Component {...pageProps} />
+  </div>
 }
