de2301ba7748a81903ff661ac83c77474c1fbad9
knowdeep <worstgrim@gmail.com>
2023-05-30 23:31:26 +0700

    fix: test icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,12 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Active On Holiday</title>
         <meta name="description" content="Ez Web Page" />
-        <link rel="shortcut icon" type="image/png" href="pixel-icon-cat.png"/>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="./pixel-icon-cat.png"
+        />
         <link rel="stylesheet" href="./node_modules/nes.css/css/nes.min.css" />
         <link href="https://unpkg.com/nes.css/css/nes.css" rel="stylesheet" />
       </Head>
